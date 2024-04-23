bebe2a2657c5f3ef3eb8706af95c6792db2d0478
Fpierr <fanuelp9@gmail.com>
2024-04-24 01:14:21 +0200

    sum list union int and float annotate variable

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""mplex types - mixed list"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """Calculates the sum of a list of integers and floats"""
+    return sum(mxd_lst)
