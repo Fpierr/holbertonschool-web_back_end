eae97d0f8681479841cb53475ab499d06584d7e7
Fpierr <fanuelp9@gmail.com>
2024-04-24 01:41:47 +0200

    iterable sequence and tuple list

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -5,4 +5,5 @@ from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Iterable and Sequence from the typing module"""
     return [(i, len(i)) for i in lst]
