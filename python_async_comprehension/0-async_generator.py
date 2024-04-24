ff56eadc1e0538d1e62d182d4e8f50a0146081f5
Fpierr <fanuelp9@gmail.com>
2024-04-24 04:40:46 +0200

    asyncio random sleep uniform generate number from 0 to 10

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ from random uniform
 from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Coroutine that generate 10 random number enter 0 to 10."""
     for _ in range(10):
         await sleep(1)
