aa98b0b9b0cef2c9826cf4aa150928b1d2d3da17
Fpierr <fanuelp9@gmail.com>
2024-05-15 11:07:42 +0200

    0-promise.js

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,9 +1,5 @@
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
+function getResponseFromAPI() {
+  return Promise.resolve();
 }
+
+export default getResponseFromAPI;
