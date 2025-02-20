7ca56795d0011f188ae5d009c2c68285b4af8145
Fpierr <fanuelp9@gmail.com>
2025-02-20 07:51:37 +0100

    add lifo cache

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -3,9 +3,9 @@
 
 from base_caching import BaseCaching
 
+
 class LIFOCache(BaseCaching):
     """ LIFO Caching system"""
-
 
     def __init__(self):
         """initialize the class"""
@@ -15,11 +15,11 @@ class LIFOCache(BaseCaching):
         """Assign an item to the cache using LIFO policy"""
         if key is None or item is None:
             return
-        
+
         self.cache_data[key] = item
-        
+
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            last_key =list(self.cache_data.keys())[-2]
+            last_key = list(self.cache_data.keys())[-2]
             print(f"DISCARD: {last_key}")
             del self.cache_data[last_key]
 
