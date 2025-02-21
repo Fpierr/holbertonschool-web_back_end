97e2556e8bbf0f080d091b329e4b0819cb33ce72
Fpierr <fanuelp9@gmail.com>
2025-02-21 12:55:01 +0100

    connect to a mysql database

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,6 +5,8 @@ import re
 from typing import List
 from typing import Tuple
 import logging
+import os
+import mysql.connector
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
@@ -50,3 +52,14 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """connect to the database using environnement variables"""
+    db_connection = mysql.connector.connect(
+        user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root"),
+        password = os.getenv("PERSONAL_DATA_DB_PASSWORD", ""),
+        host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
+        database = os.getenv("PERSONAL_DATA_DB_NAME")
+    )
+    return db_connection
