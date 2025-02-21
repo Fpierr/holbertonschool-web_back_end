23f63145d685e532afd75822015c8a68b83b637b
Fpierr <fanuelp9@gmail.com>
2025-02-21 09:30:32 +0100

    update class redacting formater

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -23,12 +23,13 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: Tuple[str]):
+    def __init__(self, fields: List[str]):
+        """ Constructor Method """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """Format log record with redacted sensitive information"""
-        record.msg = filter_datum(
-            self.fields, self.REDACTION, record.msg, self.SEPARATOR)
-        return super().format(record)
+        """ Filters values in incoming log records using filter_datum """
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
