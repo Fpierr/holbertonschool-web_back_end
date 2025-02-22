32c57001a1975013b7f5398f87197fc7aacbcd8d
Fpierr <fanuelp9@gmail.com>
2025-02-22 23:54:31 +0100

    add updated

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -8,18 +8,24 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """ Not found handler """
     return jsonify({"error": "Not found"}), 404
 
+@app.errorhandler(401)
+def unauthorized_error(error):
+    """ Handler for 401 Unauthorized error """
+    return jsonify({"error": "Unauthorized"}), 401
+
+@app.route('/api/v1/unauthorized', methods=['GET'])
+def trigger_unauthorized():
+    """ Route to trigger a 401 Unauthorized error """
+    abort(401)
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
