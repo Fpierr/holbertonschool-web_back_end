1c0c8fc9a21716637fcb07683de0f99266a949b7
Fpierr <fanuelp9@gmail.com>
2025-02-23 20:43:25 +0100

    add empty class session auth

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Session Auth"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Session authentication class, inheriting from Auth.
+    Currently empty as a placeholder for future implementation.
+    """
+    pass
