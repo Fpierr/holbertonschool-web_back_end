2fb119cc5c2d85c8f008fac718a00bc94ec8a714
Fpierr <fanuelp9@gmail.com>
2025-02-23 21:08:19 +0100

    add session cookie

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth:
@@ -29,3 +30,12 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """returns None - request will be the Flask request object"""
         return None
+
+    def session_cookie(self, request=None):
+        """Retrieve the session cookie value from the request"""
+        if request is None:
+            return None
+        session_name = getenv("SESSION_NAME")
+        if session_name is None:
+            return None
+        return request.cookies.get(session_name)
