535915e2668a3459af82dd4303b6e870a8ac820d
Fpierr <fanuelp9@gmail.com>
2025-02-23 01:07:31 +0100

    edit

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
       - 401 error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden():
+    """ GET /api/v1/forbidden
+    Raise:
+      - 403 Forbidden error
+    """
+    abort(403)
