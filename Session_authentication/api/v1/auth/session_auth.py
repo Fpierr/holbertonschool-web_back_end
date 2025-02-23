86ea43105612c454cc7db13ac249c73154816a39
Fpierr <fanuelp9@gmail.com>
2025-02-23 21:03:22 +0100

    create instance

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -23,3 +23,18 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Retrieve the User ID based on the given Session ID.
+
+        Args:
+            session_id (str): The session ID.
+
+        Returns:
+            str: The associated User ID, or None if not found.
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
