097ed20e476e1343135c48124f88abdfd1f1b983
Fpierr <fanuelp9@gmail.com>
2025-02-23 12:25:40 +0100

    request validation

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -7,10 +7,34 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+AUTH_TYPE = getenv("AUTH_TYPE")
+
+if AUTH_TYPE == 'auth':
+    auth = Auth()
+
+
+@app.before_request
+def before_request():
+    """Filter requests before handling them."""
+    if auth is None:
+        return
+
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 @app.errorhandler(404)
@@ -25,16 +49,16 @@ def unauthorized_error(error):
     return jsonify({"error": "Unauthorized"}), 401
 
 
-@app.route('/api/v1/unauthorized', methods=['GET'])
-def trigger_unauthorized():
-    """ Route to trigger a 401 Unauthorized error """
-    abort(401)
-
-
 @app.errorhandler(403)
 def forbidden_error(error):
     """Handler for 403 Forbidden error"""
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.route('/api/v1/unauthorized', methods=['GET'])
+def trigger_unauthorized():
+    """ Route to trigger a 401 Unauthorized error """
+    abort(401)
 
 
 if __name__ == "__main__":
