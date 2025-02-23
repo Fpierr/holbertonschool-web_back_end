2109cb13edf842d7a092946d808964152913e008
Fanuel PIERRE <141949137+Fpierr@users.noreply.github.com>
2025-02-23 22:15:45 +0100

    Update session_auth.py

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
-"""Session authentication views"""
-
+""" Module of Session authentication views
+"""
 from api.v1.views import app_views
-from flask import jsonify, request, abort, make_response
+from flask import abort, jsonify, request
 from models.user import User
 from os import getenv
 
 
-@app_views.route('/api/v1/auth_session/login', methods=['POST'],
-                 strict_slashes=False)
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
+    """ POST /auth_session/login
+    Return
+        - Logged in user
+    """
     email = request.form.get('email')
 
     if not email:
@@ -43,3 +46,20 @@ def login():
     response.set_cookie(SESSION_NAME, session_id)
 
     return response
+
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ DELETE /auth_session/logout
+    Return:
+        - Empty dictionary if succesful
+    """
+    from api.v1.app import auth
+
+    deleted = auth.destroy_session(request)
+
+    if not deleted:
+        abort(404)
+
+    return jsonify({}), 200
