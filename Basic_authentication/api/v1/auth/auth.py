b82565f251f170c265c05c3ccf570b143ab318fb
Fpierr <fanuelp9@gmail.com>
2025-02-23 11:54:28 +0100

    create auth

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -10,10 +10,18 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns False - path and excluded_paths"""
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if not path.endswith("/"):
+            path += "/"
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """returns None - request will be the Flask request"""
+        if request is None:
+            return None
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
