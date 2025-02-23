455b82465921527c476a5609c15d4887997f47f6
Fpierr <fanuelp9@gmail.com>
2025-02-23 22:25:11 +0100

    edit for logout

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -55,3 +55,19 @@ class SessionAuth(Auth):
 
         user = User.get(user_id)
         return user
+
+    def destroy_session(self, request=None):
+        """Deletes the session of a user (log out)."""
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+        if not user_id:
+            return False
+
+        del self.user_id_by_session_id[session_id]
+        return True
