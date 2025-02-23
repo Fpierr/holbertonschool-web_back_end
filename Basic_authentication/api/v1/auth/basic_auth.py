15dd0beaff10786b44a9b85ccc739a4d84cff9b1
Fpierr <fanuelp9@gmail.com>
2025-02-23 13:03:49 +0100

    add edit

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,10 +2,13 @@
 """Basic auth"""
 
 from api.v1.auth.auth import Auth
-
+from base64 import b64decode
+from models.user import User
+from typing import TypeVar
 
 class BasicAuth(Auth):
     """Basic authentication class"""
+
 
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
