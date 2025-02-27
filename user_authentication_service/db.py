06bfce94d55863e5a12c7f8bf943fcb4a6004ca6
Fpierr <fanuelp9@gmail.com>
2025-02-27 18:00:59 +0100

    add db

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-from user import Base
+from user import Base, User
 
 
 class DB:
@@ -29,9 +29,8 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-
-def add_user(self, email: str, hashed_password: str) -> User:
-    new_user = User(email=email, hashed_password=hashed_password)
-    self._session.add(new_user)
-    self._session.commit()
-    return new_user
+    def add_user(self, email: str, hashed_password: str) -> User:
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
