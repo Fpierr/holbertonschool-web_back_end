03ec721af54b3426626ec2e8b06b04a0e6e5e3d0
Fpierr <fanuelp9@gmail.com>
2025-02-28 20:59:31 +0100

    edit

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,52 +1,53 @@
 #!/usr/bin/env python3
-"""DB module
-"""
+""" Database for ORM """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-from typing import TypeVar
-from user import Base, User
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
+from typing import TypeVar
+from user import Base, User
 
 
 class DB:
-    """DB class
-    """
+    """ DB Class for Object Reational Mapping """
 
-    def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+    def __init__(self):
+        """ Constructor Method """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object
-        """
+    def _session(self):
+        """ Session Getter Method """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """method to add user"""
+        """ Adds user to database
+
+        Return: User Object
+        """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """Return the first row in users table matching the filter"""
+        """ Finds user by key word args
+        Return: First row found in the users table as filtered by kwargs
+        """
         if not kwargs:
             raise InvalidRequestError
 
-        column_key = User.__table__.columns.keys()
+        column_names = User.__table__.columns.keys()
         for key in kwargs.keys():
-            if key not in column_key:
+            if key not in column_names:
                 raise InvalidRequestError
 
         user = self._session.query(User).filter_by(**kwargs).first()
