831e2fb1bedc9c31655d988bf8647399e105724c
Fpierr <fanuelp9@gmail.com>
2025-03-02 21:03:30 +0100

    add edit

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -118,3 +118,5 @@ class Auth:
         self._db.update_user(user.id,
                              hashed_password=hashed_password,
                              reset_token=None)
+
+        return None
