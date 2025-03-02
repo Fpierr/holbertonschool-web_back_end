9fb03cb650aa161eb61c1317de6c4993be20f077
Fpierr <fanuelp9@gmail.com>
2025-03-02 15:13:15 +0100

    create session and return uuid

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -67,3 +67,15 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
 
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union[str, None]:
+        """Find user by session ID"""
+        if session_id is None or user is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+
+        return user
