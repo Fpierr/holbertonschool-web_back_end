21d96d8bde71c0f05d23316739ca7cb18932f0aa
Fpierr <fanuelp9@gmail.com>
2025-03-02 15:23:48 +0100

    add auth

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -75,6 +75,8 @@ class Auth:
 
         try:
             user = self._db.find_user_by(session_id=session_id)
-            return user
+
         except NoResultFound:
             return None
+
+        return user
