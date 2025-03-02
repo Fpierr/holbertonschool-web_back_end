6b3d36355f3c488d80802d564ab6b3767e734783
Fpierr <fanuelp9@gmail.com>
2025-03-02 21:35:41 +0100

    add edit

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -114,7 +114,7 @@ class Auth:
         except NoResultFound:
             raise ValueError
 
-        hashed_password = _hash_password(password)
+        hashed_password = _hash_password(password).decode()
         self._db.update_user(user.id,
                              hashed_password=hashed_password,
                              reset_token=None)
