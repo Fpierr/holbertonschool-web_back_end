29544bc5914d8ffdb7d2f310290142c2b51f0efb
Fpierr <fanuelp9@gmail.com>
2025-03-14 12:09:54 +0100

    add exception text

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,14 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """ Test the method returns what it is supposed to """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        Teste si KeyError est levée pour des chemins invalides.
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
