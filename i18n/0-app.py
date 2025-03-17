a329d29e20d48b6f7d201e462567103b9d724c4e
Fpierr <fanuelp9@gmail.com>
2025-03-17 08:51:25 +0100

    add files

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
-"""Create simple route"""
+""" create simple route """
+
 
 from flask import Flask, request, render_template
-import os
+from os import getenv
 
 app = Flask(__name__, static_url_path='')
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
-    """Get the principale route '/' and return html"""
+    """ Get simple route and return html
+    """
     return render_template('0-index.html')
 
 
 if __name__ == "__main__":
-    host = os.getenv("API_HOST", "0.0.0.0")
-    port = os.getenv("API_PORT", "5000")
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
