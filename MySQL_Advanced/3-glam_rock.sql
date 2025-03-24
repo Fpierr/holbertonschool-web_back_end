6844817e7b8c5c8cb0a64f3ffca88aad8930c713
Fpierr <fanuelp9@gmail.com>
2025-03-24 09:41:18 +0100

    add sql file

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Task 3
 -- Glam rock
-SELECT band_name, COALESCE(YEAR(split), YEAR(CURDATE())) - formed AS lifespan
+SELECT band_name, COALESCE(split, 2025) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
