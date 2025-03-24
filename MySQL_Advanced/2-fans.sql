565dbd077fe2e6707e9d614341e8754910b11cab
Fpierr <fanuelp9@gmail.com>
2025-03-24 09:29:40 +0100

    add sql file

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,5 +1,7 @@
 -- Task 2
+-- cat metal_bands.sql | mysql -uroot -p holberton
 -- cat 2-fans.sql | mysql -uroot -p holberton > tmp_res ; head tmp
-SELECT DISTINCT `origin`, SUM(`fans`) as `nb_fans` FROM `metal_bands`
+SELECT DISTINCT `origin`, SUM(`fans`) as `nb_fans`
+FROM `metal_bands`
 GROUP BY `origin`
 ORDER BY `nb_fans` DESC;
