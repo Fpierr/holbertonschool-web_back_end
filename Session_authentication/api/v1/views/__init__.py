4b3f348cacc66230d19a974379939c121ded5d27
Fpierr <fanuelp9@gmail.com>
2025-07-29 19:05:31 +0200

    add coreection

--- Session_authentication/api/v1/views/__init__.py
+++ Session_authentication/api/v1/views/__init__.py
@@ -1,13 +1,9 @@
-#!/usr/bin/env python3
-"""Initialize API v1 blueprint and routes.
-"""
-
 from flask import Blueprint
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
 from api.v1.views.index import *
 from api.v1.views.users import *
 from api.v1.views.session_auth import *
-from models.user import User
-
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 User.load_from_file()
